35c1d2c23c1b669253f8127f89b677363bf7ee8e
Anibal Corral Serrano <anibal.corral@gmail.com>
2022-08-18 12:54:20 -0400

    Create the empty app

--- lib/hangman.ex
+++ lib/hangman.ex
@@ -1,18 +1,8 @@
 defmodule Hangman do
-  @moduledoc """
-  Documentation for `Hangman`.
-  """
+def new_game do
 
-  @doc """
-  Hello world.
+end
+def make_move(game, guess) do
 
-  ## Examples
-
-      iex> Hangman.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
+end
 end
