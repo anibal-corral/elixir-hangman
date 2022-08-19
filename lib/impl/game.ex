aa96bd2b8c1fdf6602e59ff1abbf40f887cc3dd5
Anibal Corral Serrano <anibal.corral@gmail.com>
2022-08-19 14:20:46 -0400

    Handled bad guess

--- lib/impl/game.ex
+++ lib/impl/game.ex
@@ -61,7 +61,32 @@ defmodule Hangman.Impl.Game do
     # Generate a new map based on the current game but we add the letter (guess)
     # to the used letter in the game
     %{ game | used: MapSet.put(game.used, guess)}
+    |> score_guess(Enum.member?(game.letters, guess))
   end
+  #If we have a good guess, this function will be executed
+  defp score_guess(game, true) do
+    # If we guessed all letter, the game is won else is a good guess
+    new_state = maybe_won(MapSet.subset?(MapSet.new(game.letters), game.used))
+    %{ game | game_state: new_state }
+  end
+
+  # This function is executed when turns left is equal 1
+  defp score_guess(game = %{ turns_left: 1 }, false) do
+    %{ game | game_state: :lost, turns_left: 0}
+  end
+  # This function is executed when turns_left is different to 1
+  defp score_guess(game, false) do
+    %{ game | game_state: :bad_guess, turns_left: game.turns_left - 1 }
+  end
+
+
+
+  defp maybe_won(true), do: :won
+  defp maybe_won(_), do: :good_guess
+
+
+
+
   #
   defp return_with_tally(game) do
     {game, tally(game)}
@@ -71,9 +96,16 @@ defmodule Hangman.Impl.Game do
      %{
       turns_left: game.turns_left,
       game_state: game.game_state,
-      letters: [],
+      letters: reveal_guessed_letters(game),
       used: game.used |> MapSet.to_list |> Enum.sort()
     }
   end
+
+  defp reveal_guessed_letters(game)do
+    game.letters
+    |> Enum.map(fn letter -> MapSet.member?(game.used, letter) |> maybe_reveal(letter) end )
+  end
+  defp maybe_reveal(true, letter), do: letter
+  defp maybe_reveal(false, _letter), do: "_"
 
 end
