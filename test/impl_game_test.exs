ad21d8292308c2ee5957e6190166f67f44ff3fbc
Anibal Corral Serrano <anibal.corral@gmail.com>
2022-08-19 10:40:52 -0400

    Make_move if won

--- test/impl_game_test.exs
+++ test/impl_game_test.exs
@@ -19,9 +19,14 @@ alias Hangman.Impl.Game
   end
   test "New game returns each letters is lowercase" do
     game = Game.new_game()
+    assert Enum.join(game.letters) == String.downcase(Enum.join(game.letters))
+  end
 
-assert Enum.join(game.letters) == String.downcase(Enum.join(game.letters))
-
+  test "state doesn't change if a game is won" do
+    game = Game.new_game("Anais")
+    game = Map.put(game, :game_state, :won)
+    { new_game, tally } = Game.make_move(game, "x")
+    assert new_game == game
   end
 
 end
