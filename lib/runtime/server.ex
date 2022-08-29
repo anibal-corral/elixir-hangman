98373e07ba7a29abc66d76fb3a41ba84cec645e7
Anibal Corral Serrano <anibal.corral@gmail.com>
2022-08-29 12:06:10 -0400

    Making hangman into a server

---
+++ lib/runtime/server.ex
@@ -0,0 +1,27 @@
+defmodule Hangman.Runtime.Server do
+  use GenServer
+  alias Hangman.Impl.Game
+
+  # THis is for starting the generic server
+  #Client Process
+
+  def start_link do
+    GenServer.start_link(__MODULE__, nil)
+  end
+
+  #Server process
+  def init(_) do
+    #the Job is returning a tuple with the ok and the state
+    {:ok, Game.new_game} #Here the state is the hangman game
+
+  end
+def handle_call({:make_move, guess}, _from, state) do
+  { updated_game, tally } = Game.make_move(state, guess)
+  {:reply, tally, updated_game}
+end
+
+def handle_call({:tally}, _from, state) do
+  {:reply, Game.tally(state), state}
+end
+
+end
