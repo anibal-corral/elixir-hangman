4d0b5e47d34d3a8fe70ccda54f97d3507a6fb1fe
Anibal Corral Serrano <anibal.corral@gmail.com>
2022-08-29 12:32:49 -0400

    Adding an API to the genserver

--- lib/hangman.ex
+++ lib/hangman.ex
@@ -3,19 +3,28 @@ defmodule Hangman do
 
   alias Hangman.Impl.Game, as: Game
   alias Hangman.Type, as: Type
+  alias Hangman.Runtime.Server
 
-  @opaque game:: Game.t
+  @opaque game:: Server.t
 
 @spec new_game() :: game
 # This line replace the next def ...
-defdelegate new_game, to: Game
+# defdelegate new_game, to: Game
 # def new_game do
 #   Game.new_game()
 # end
+def new_game() do
+  {:ok, pid} = Server.start_link()
+  pid
+end
 @spec make_move(game, String.t) :: { game, Type.tally }
-defdelegate make_move(game, guess), to: Game
+# defdelegate make_move(game, guess), to: Game
 # def make_move(game, guess) do
 # end
+
+def make_move(game, guess) do
+  GenServer.call(game, {:make_move, guess})
+end
 
 @spec sum(integer, integer) :: integer
 def sum(a,b) do
@@ -23,5 +32,8 @@ def sum(a,b) do
 end
 
 @spec tally(game) :: Type.tally()
-defdelegate tally(game), to: Game
+# defdelegate tally(game), to: Game
+def tally(game)  do
+  GenServer.call(game, {:tally})
+end
 end
